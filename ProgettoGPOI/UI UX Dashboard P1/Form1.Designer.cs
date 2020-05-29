73610fefb0910b7c7ebd6db58fcdd52523a4f6e3
simonegerbaudo <57095426+simonegerbaudo@users.noreply.github.com>
2020-05-29 21:51:15 +0200

    Add files via upload

---
+++ ProgettoGPOI/UI UX Dashboard P1/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace UI_UX_Dashboard_P1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1361, 47);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label3.Location = new System.Drawing.Point(1322, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "X";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(245, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SCONTRINO ELETTRONICO";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1361, 741);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ ProgettoGPOI/UI UX Dashboard P1/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace UI_UX_Dashboard_P1
+{
+    public partial class Form1 : Form
+    {
+        private void Crea(string progressivo, string formato,
+              string tipo,
+              string dA,
+              string idDispositivo,
+              string latGeo,
+              string longGeo,
+              string dal,
+              string al,
+              string dataOraRilevazione,
+              string aliquotaIVA,
+              string imposta,
+              string natura,
+              string ventilazioneIVA,
+              string ammontare,
+              string rifNormativo,
+              string importoParziale,
+              string totaleAmmontareResi,
+              string totaleAmmontareAnnulli,
+              string beniInSospeso,
+              string nonRiscossoServizi,
+              string nonRiscossoFatture,
+              string totaleDaFattureRT,
+              string nonRiscossoDCRaSSN,
+              string nonRiscossoOmaggio,
+              string codiceAttivita,
+              string numeroDocCommerciali,
+              string pagatoContanti,
+              string pagatoElettronico,
+              string scontoApagare,
+              string pagatoTicket,
+              string numeroTicket,
+              string venduto,
+              string vendutoContante,
+              string vendutoNoContante,
+              string incassato,
+              string incassatoRicarica,
+              string incassatoVendita,
+              string totaleResoTubiResto,
+              string totaleCaricatoTubiResto,
+              string totaleResoManualeTubiResto,
+              string totaleCaricatoManualeTubiResto,
+              string dataOraPrelievoPrec,
+              string progressivoPrelievo,
+              string vendutoC,
+              string vendutoContanteC,
+              string vendutoNoContanteC,
+              string incassatoC,
+              string incassatoRicaricaC,
+              string incassatoVenditaC,
+              string totaleResoTubiRestoC,
+              string totaleCaricatoTubiRestoC,
+              string totaleResoManualeTubiRestoC,
+              string totaleCaricatoManualeTubiRestoC,
+              string cFTecnico,
+              string idPaese,
+              string idCodice,
+              string dataOra,
+              string codice,
+              string note,
+              string numeroSw,
+              string dataRelease,
+              string matricola,
+              string dataOraS,
+              string codiceS,
+              string noteS)
+        {
+            XDocument xDoc = new XDocument(
+                 new XDeclaration("1.0", "utf-8", "yes"),
+                 new XElement("DatiCorrispettivi", //Create the Root Element with specified Attributes such as Name
+                     new XAttribute("versione", "COR10"),
+
+                 new XElement("Trasmissione", //Create first Child that has mutliple Attributes
+                      new XElement("Progressivo", progressivo),
+                      new XElement("Formato", formato),
+                      new XElement("Dispositivo",
+                            new XElement("Tipo",tipo),
+                            new XElement("IdDispositivo",idDispositivo),
+                            new XElement("GeoLocalizzazione",
+                                new XElement("Lat",latGeo),
+                                new XElement("Long", longGeo)) 
+                      )
+                 ),
+
+                new XElement("PeriodoInattivo", //Create first Child that has mutliple Attributes
+                      new XElement("Dal", dal),
+                      new XElement("Al", al)
+                 ),
+
+                new XElement("DataOraRilevazione", dataOraRilevazione),
+
+                new XElement("DatiRT", //Create first Child that has mutliple Attributes
+                      new XElement("Riepilogo",
+                      new XElement("IVA",
+                            new XElement("AliquotaIVA", aliquotaIVA),
+                            new XElement("Imposta", imposta)
+                      ),
+                      new XElement("Natura", natura),
+                      new XElement("VentilazioneIVA", ventilazioneIVA),
+                      new XElement("Ammontare", ammontare),
+                      new XElement("RifNormativo", rifNormativo),
+                      new XElement("ImportoParziale", importoParziale),
+                      new XElement("RifNormativo", rifNormativo),
+                      new XElement("TotaleAmmontareResi", totaleAmmontareResi),
+                      new XElement("TotaleAmmontareAnnulli", totaleAmmontareAnnulli),
+                      new XElement("BeniInSospeso", beniInSospeso),
+                      new XElement("NonRiscossoServizi", nonRiscossoServizi),
+                      new XElement("NonRiscossoFatture", nonRiscossoFatture),
+                      new XElement("TotaleDaFattureRT", totaleDaFattureRT),
+                      new XElement("NonRiscossoDCRaSSN", nonRiscossoDCRaSSN),
+                      new XElement("NonRiscossoOmaggi", nonRiscossoOmaggio),
+                      new XElement("CodiceAttivita", codiceAttivita),
+                      new XElement("Totali",
+                            new XElement("NumeroDocCommerciali", numeroDocCommerciali),
+                            new XElement("PagatoContanti", pagatoContanti),
+                            new XElement("PagatoElettronico", pagatoElettronico),
+                            new XElement("ScontoApagare", scontoApagare),
+                            new XElement("Ticket",
+                                new XElement("PagatoTicket", pagatoTicket),
+                                new XElement("NumeroTicket", numeroTicket))
+                      ))
+                 ),
+
+
+
+                new XElement("DatiDA", //Create first Child that has mutliple Attributes
+                      new XElement("Periodo",
+                        new XElement("Venduto", venduto),
+                        new XElement("VendutoContante", vendutoContante),
+                        new XElement("VendutoNoContante", vendutoNoContante),
+                        new XElement("Incassato", incassato),
+                        new XElement("IncassatoRicarica", incassatoRicarica),
+                        new XElement("IncassatoVendita", incassatoVendita),
+                        new XElement("TotaleResoTubiResto", totaleResoManualeTubiResto),
+                        new XElement("TotaleCaricatoTubiResto", totaleCaricatoManualeTubiResto),
+                        new XElement("TotaleResoManualeTubiResto", totaleResoManualeTubiResto),
+                        new XElement("DataOraPrelievoPrec", dataOraPrelievoPrec),
+                        new XElement("ProgressivoPrelievo", progressivoPrelievo)
+                      ),
+                      new XElement("Cumulato",
+                        new XElement("Venduto", vendutoC),
+                        new XElement("VendutoContante", vendutoContanteC),
+                        new XElement("VendutoNoContante", vendutoNoContanteC),
+                        new XElement("Incassato", incassatoC),
+                        new XElement("IncassatoRicarica", incassatoRicaricaC),
+                        new XElement("IncassatoVendita", incassatoVenditaC),
+                        new XElement("TotaleResoTubiResto", totaleResoManualeTubiRestoC),
+                        new XElement("TotaleCaricatoTubiResto", totaleCaricatoManualeTubiRestoC),
+                        new XElement("TotaleResoManualeTubiResto", totaleResoManualeTubiRestoC)
+                      )
+                 ),
+
+                new XElement("InterventoTecnico", //Create first Child that has mutliple Attributes
+                      new XElement("CFTecnico", cFTecnico),
+                      new XElement("IdIVALaboratorio",
+                        new XElement("IdPaese", idPaese),
+                        new XElement("IdCodice", idCodice)
+                      ),
+                     new XElement("DataOra", dataOra),
+                     new XElement("Codice", codice),
+                     new XElement("Note", note),
+                     new XElement("NumeroSw", numeroSw),
+                     new XElement("DataRelease", dataRelease)
+                 ),
+                 new XElement("Segnalazione",
+                     new XAttribute("Matricola", matricola),
+                     new XAttribute("DataOra", dataOraS),
+                     new XAttribute("Codice", codiceS),
+                     new XAttribute("Note", noteS)
+                 ) //the second string is the value between brackets
+
+                     ) //end of Root Element
+                 );
+            //save the data into a file name/path.
+            //xDoc.Save(path);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel8_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
